a68055c3194f46bb27766323359fd1802340527e
Andrew Branch <andrew@wheream.io>
2022-09-27 11:41:18 -0700

    Don’t try to make tags that already exist

--- make-git-tags-for-new-builds.js
+++ make-git-tags-for-new-builds.js
@@ -15,16 +15,12 @@ const exec = (cmd, opts) => {
 };
 
 const go = async () => {
-  // So we can see what is already available
-  const releases = await get("https://tswebinfra.blob.core.windows.net/indexes/releases.json")
-  const preReleases = await get("https://tswebinfra.blob.core.windows.net/indexes/pre-releases.json")
-
-  const all = [...releases.versions, ...preReleases.versions]
+  const localTags = exec("git tag", { encoding: "utf8" }).split("\n")
 
   // e.g { "latest":"4.0.3","next":"4.1.0-dev.20201026","beta":"4.1.0-beta","rc":"4.0.1-rc","insiders":"4.0.2-insiders.20200818","tag-for-publishing-older-releases":"3.6.5","dev":"3.9.4" }
-  const tags = await get("https://registry.npmjs.org/-/package/typescript/dist-tags")
-  const needed = [tags.latest, tags.beta, tags.rc]
-  const todo = needed.filter(tag => !all.includes(tag))
+  const distTags = await get("https://registry.npmjs.org/-/package/typescript/dist-tags")
+  const needed = [distTags.latest, distTags.beta, distTags.rc]
+  const todo = needed.filter(tag => !localTags.includes(tag))
   if (todo.length === 0) {
     console.log("No new builds to create.")
     return
